9eec25ed776b182352af1b96a7d457ba5a9bdc62
Muhannadjz <muhannadzowayed05@gmail.com>
2025-08-05 01:17:35 +0300

    adding authentication route

---
+++ routes/auth.routes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+// Sign up page
+router.get('/sign-up', (req, res) => {
+  res.render('auth/sign-up');
+});
+
+// Sign up logic
+router.post('/sign-up', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, password: hashedPassword });
+  await newUser.save();
+  res.redirect('/login');
+});
+
+// Login page
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+// Login logic
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user && await bcrypt.compare(password, user.password)) {
+    req.session.userId = user._id;
+    res.redirect('/recipes');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+// Logout logic
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+module.exports = router;
